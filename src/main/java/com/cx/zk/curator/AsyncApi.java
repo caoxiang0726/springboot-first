63d7f94ce29155169b337147719020ed7ee43e03
caoxiang <xiang.cao@teradata.com>
2018-09-05 09:30:14 +0800

    curator sence

---
+++ src/main/java/com/cx/zk/curator/AsyncApi.java
@@ -0,0 +1,7 @@
+package com.cx.zk.curator;
+
+/**
+ * Created by caoxiang on 2018/9/4.
+ */
+public class AsyncApi {
+}
---
+++ src/main/java/com/cx/zk/curator/DemoSence.java
@@ -0,0 +1,29 @@
+package com.cx.zk.curator;
+
+/**
+ * curator对zk使用的典型场景精选封装
+ * 引入 recipes包
+ */
+public class DemoSence {
+
+    /**
+     * 事件监听：比如节点的值发生变化
+     * 原生api需要反复注册watcher
+     */
+
+    public static void nodeCache(){
+
+    }
+
+
+    /**
+     * master选举
+     */
+
+
+    /**
+     * 分布式锁
+     */
+
+
+}
